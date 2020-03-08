3781c7e78811e05a9aa71789b71bc6b78d631b0e
Yuri Partitski <PartySky@users.noreply.github.com>
2020-03-08 03:01:12 +0200

    Update MainController.cs

--- VirtualServer/VirtualServer/Controllers/MainController.cs
+++ VirtualServer/VirtualServer/Controllers/MainController.cs
@@ -1,10 +1,13 @@
 ﻿﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+ using Google.Protobuf.WellKnownTypes;
+ using Microsoft.AspNetCore.Mvc;
 using VirtualServer.Entities;
+using VirtualServer.Models;
 
-namespace VirtualServer.Controllers
+ namespace VirtualServer.Controllers
 { 
     [Route("api")]    
     public class MainController : Controller
@@ -16,22 +19,139 @@ namespace VirtualServer.Controllers
             _db = db;
         }
 
-        [HttpGet, Route("test")]
-        public Task<JsonResult> Test()
+        [HttpGet, Route("")]
+        public Task<JsonResult> GetServers()
         {
             return Task.Run(() =>
             {
+                var result = new List<Server>();
                 try
                 {
-                    var xTest = _db.Servers.ToList();
+                    result = _db.Servers.ToList();
                 }
                 catch(Exception e)
                 {
                     Console.WriteLine(e.Message);
                 }
 
-                return Json("Test");
+                return Json(result);
             });
+        }
+        
+        [HttpGet, Route("add")]
+        public Task<JsonResult> AddServer()
+        {
+            return Task.Run(() =>
+            {
+                var result = new Server();
+                var dateNow = DateTime.Now;
+                result.CreateTime = dateNow;
+                try
+                {
+                    ProcessLastStartDate(dateNow);
+                    _db.Servers.Add(result);
+                    _db.SaveChanges();
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                return Json(result);
+            });
+        }
+        
+        
+        [HttpPost, Route("removeall")]
+        public Task<JsonResult> RemoveAll([FromBody] int[] param)
+        {
+            return Task.Run(() =>
+            {
+                var result = new List<Server>();
+                
+                var dateNow = DateTime.Now;
+                
+                try
+                {   
+                    result = _db.Servers
+                        .Where(p => param.Any(item => item == p.Id))
+                        .ToList();
+
+                    foreach (var item in result)
+                    {
+                        item.IsRemoved = true;
+                        item.RemovedTime = dateNow;
+                    }
+                    
+                    _db.Servers.UpdateRange(result);
+                    _db.SaveChanges();
+                    
+                    // TODO: check if these removing servers are the last ones
+                    // Now one can click remove button and reset last stop time even
+                    // when there aren no running servers
+                    ProcessLastStopDate(dateNow);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                return Json(result);
+            });
+        }
+        
+        [HttpGet, Route("gettotalusetime")]
+        public Task<JsonResult> GetTotalUseTime()
+        {
+            return Task.Run(() =>
+            {
+                var result = new TimeSpan();
+                var dateNow = DateTime.Now;
+
+                try
+                {
+                    result = GetUsageTime(dateNow);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                return Json(result);
+            });
+        }
+
+        private void ProcessLastStartDate(DateTime dateNow)
+        {
+            if (IsAnyRunningServrer()) { return; }
+
+            var result = _db.UsageTimes.FirstOrDefault();
+            result.LastStartDate = dateNow;
+            _db.Update(result);
+            _db.SaveChanges();
+        }
+        
+        private void ProcessLastStopDate(DateTime dateNow)
+        {
+            if (IsAnyRunningServrer()) { return; }
+
+            var result = _db.UsageTimes.FirstOrDefault();
+            result.LastStopDate = dateNow;
+            
+            var nonNullableTimeSpanTemp = new TimeSpan(); 
+            
+            nonNullableTimeSpanTemp = nonNullableTimeSpanTemp
+                .Add((TimeSpan) (result.LastStopDate - result.LastStartDate));
+
+            if (result.TotalUsageTime == null)
+            {
+                result.TotalUsageTime = new DateTime(0001, 01, 01);
+            }
+
+            result.TotalUsageTime = result.TotalUsageTime + nonNullableTimeSpanTemp;
+            
+            _db.Update(result);
+            _db.SaveChanges();
         }
 
         private TimeSpan GetUsageTime(DateTime dateNow)
@@ -49,6 +169,13 @@ namespace VirtualServer.Controllers
             }
 
             return result;
+        }
+
+        private bool IsAnyRunningServrer()
+        {
+            var runningServers = _db.Servers
+                .FirstOrDefault(p => !p.IsRemoved);
+            return runningServers != null;
         }
     }
 }
